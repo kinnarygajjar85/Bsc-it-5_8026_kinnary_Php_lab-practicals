853ef200bb3c0fd18d50fa62702a3b346b337b06
kinnarygajjar85 <kinnarygajjar85@gmail.com>
2026-06-09 11:11:54 +0530

    Add files via upload

---
+++ u1_p8_4.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        error_reporting(0);
+      ?>
+            <form method="get">
+                <input type="text" name="uname0">
+                <input type="text" name="uname1">
+                <input type="text" name="uname2">
+                <input type="text" name="uname3">
+                <input type="text" name="uname4">
+                <input type="text" name="uname5">
+                
+                <input type="submit"name="submit" value="submit">
+</form>
+        <?php
+            $nm = array();
+            $nm[1]=$_GET['uname0'];
+            $nm[2]=$_GET['uname1'];
+            $nm[3]=$_GET['uname2'];
+            $nm[4]=$_GET['uname3'];
+            $nm[5]=$_GET['uname4'];
+            $nm[6]=$_GET['uname5'];
+            foreach ($nm as $n)
+            {
+                echo$n."<br>";
+            }
+            ?>
+</body>
+</html>     
